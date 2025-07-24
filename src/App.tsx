a086d60fd58c5a5fa97a981d937cf639c9201b6c
ManuelaKronen <manuela.kronen@gmail.com>
2025-07-24 10:59:48 +0200

    green background

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,12 @@
+// src/App.tsx
+function App() {
+  return (
+    <div style={{ backgroundColor: "green", height: "100vh" }}>
+      <h1 style={{ color: "white", textAlign: "center", paddingTop: "2rem" }}>
+        Green Background Test 🚀
+      </h1>
+    </div>
+  );
+}
 
+export default App;
